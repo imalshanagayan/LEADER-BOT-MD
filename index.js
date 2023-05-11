72d733376084dc23ca85cce9017cb2eb2c810787
imalshanagayan <122774908+imalshanagayan@users.noreply.github.com>
2023-05-11 12:14:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('GURU -  BOT', {
+say('LEADER-BOT-MD
+', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
